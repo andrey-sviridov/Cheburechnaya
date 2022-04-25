38dffc9ef33a96b25adc079241cb881296ca4fc4
andrey-sviridov <41750232+andrey-sviridov@users.noreply.github.com>
2022-04-25 17:49:27 +0600

    Update ModelContext.cs

--- cheburechnaya-core/cheburechnaya-core/Data/ModelContext.cs
+++ cheburechnaya-core/cheburechnaya-core/Data/ModelContext.cs
@@ -4,5 +4,9 @@ using Microsoft.EntityFrameworkCore;
 namespace cheburechnaya_core.Data {
     public class ModelContext : DbContext {
         public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TestDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
     }
 }
