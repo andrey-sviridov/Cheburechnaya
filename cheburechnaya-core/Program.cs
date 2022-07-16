4b2898653551d21bcf32f095695c35712d6c1190
andrey-sviridov <northarion@yandex.ru>
2022-07-16 18:57:57 +0600

    123

--- cheburechnaya-core/Program.cs
+++ cheburechnaya-core/Program.cs
@@ -47,7 +47,7 @@ static void ConfigureCorsPolicy(WebApplicationBuilder builder) {
 		opt.AddPolicy("CorsPolicy", policy => {
 			policy.AllowAnyHeader()
 				.AllowAnyMethod()
-				.WithOrigins("http://localhost:8080")
+				.WithOrigins("https://cheburechnaya.vercel.app/", "http://localhost:8080")
 				.AllowCredentials();
 		});
 	});
